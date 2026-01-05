70f65ea7461d4f2b11149a7a0187d902123ab50b
SangwanYu <ryusangwan12@gmail.com>
2026-01-05 12:10:01 +0900

    Design: 레이아웃 설정

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -2,9 +2,14 @@ import { Outlet } from "react-router-dom";
 
 const RootLayout = () => {
   return (
-    <>
-      <Outlet />
-    </>
+    <div className="flex min-h-svh justify-center bg-gray-100">
+      <main className="relative flex min-h-svh w-full max-w-(--width-app) flex-col overflow-x-hidden bg-white pt-safe-top pr-safe-right pb-safe-bottom pl-safe-left shadow-xl">
+        <div className="mx-auto w-full max-w-(--width-design-base) flex-1 px-layout-side">
+          <Outlet />
+        </div>
+      </main>
+    </div>
   );
 };
+
 export default RootLayout;
