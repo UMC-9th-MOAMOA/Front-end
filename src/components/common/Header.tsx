806d68b3b3965c0eeb0b8dce99c907c9ebd24b19
SangwanYu <ryusangwan12@gmail.com>
2026-01-06 15:43:14 +0900

    Feat: 헤더 뒤로가기 로직 추가, 나머지는 각 페이지에서 함수 전달

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router-dom";
 import IcLeft from "@/assets/icons/ic_left.svg?react";
 import IcMenu from "@/assets/icons/ic_menu.svg?react";
 import IcSearch from "@/assets/icons/ic_search.svg?react";
@@ -5,22 +6,41 @@ import IcSearch from "@/assets/icons/ic_search.svg?react";
 interface HeaderProps {
   title: string;
   property: "common" | "search" | "menu";
+  onBack?: () => void;
+  onSearch?: () => void;
+  onMenu?: () => void;
 }
 
-export default function Header({ title, property }: HeaderProps) {
+export default function Header({
+  title,
+  property,
+  onBack,
+  onSearch,
+  onMenu,
+}: HeaderProps) {
+  const navigate = useNavigate();
+
+  const handleBack = () => {
+    if (onBack) {
+      onBack();
+    } else {
+      navigate(-1);
+    }
+  };
+
   return (
     <header className="grid grid-cols-[1.5rem_1fr_1.5rem] items-center pt-7">
-      <button type="button" aria-label="뒤로가기">
+      <button type="button" aria-label="뒤로가기" onClick={handleBack}>
         <IcLeft className="size-6" />
       </button>
       <h1 className="heading-2 text-center text-black">{title}</h1>
       {property === "search" && (
-        <button type="button" aria-label="검색">
+        <button type="button" aria-label="검색" onClick={onSearch}>
           <IcSearch className="size-6" />
         </button>
       )}
       {property === "menu" && (
-        <button type="button" aria-label="메뉴">
+        <button type="button" aria-label="메뉴" onClick={onMenu}>
           <IcMenu className="size-6" />
         </button>
       )}
