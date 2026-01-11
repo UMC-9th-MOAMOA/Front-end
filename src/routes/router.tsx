d8fc62686c1fdfd9a521920986a04e18fefc956e
aeongiing <ywjk0515@gmail.com>
2026-01-11 21:45:05 +0900

    Feat: 라우터에 경로 추가

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,6 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
 import LoginPage from "@/pages/auth/login/Login";
 import SignUpPage from "@/pages/auth/signup/SignUp";
+import HomePage from "@/pages/home/Home";
+import MyPage from "@/pages/my/My";
+import SearchPage from "@/pages/search/Search";
+import SettingsPage from "@/pages/settings/Settings";
 import RootLayout from "../layouts/RootLayout";
 
 const router = createBrowserRouter([
@@ -8,6 +12,10 @@ const router = createBrowserRouter([
     path: "/",
     element: <RootLayout />,
     children: [
+      { index: true, element: <HomePage /> },
+      { path: "search", element: <SearchPage /> },
+      { path: "my", element: <MyPage /> },
+      { path: "settings", element: <SettingsPage /> },
       { path: "login", element: <LoginPage /> },
       { path: "signup", element: <SignUpPage /> },
     ],
