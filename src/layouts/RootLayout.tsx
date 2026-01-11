520644acf0666acabc580da2cc430f432df15317
aeongiing <ywjk0515@gmail.com>
2026-01-11 21:46:24 +0900

    Feat: 조건부 BottomNavigation을 포함한 RootLayout 추가

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,12 +1,20 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
+import BottomNavigation from "@/components/common/navbar/BottomNavbar";
 
 const RootLayout = () => {
+  const location = useLocation();
+
+  const showNavigation = ["/", "/search", "/my", "/settings"].includes(
+    location.pathname
+  );
+
   return (
     <div className="flex min-h-svh justify-center bg-gray-100">
       <main className="relative flex min-h-svh w-full max-w-(--width-app) flex-col overflow-x-hidden bg-white pt-safe-top pr-safe-right pb-safe-bottom pl-safe-left shadow-xl">
         <div className="mx-auto w-full max-w-(--width-design-base) flex-1 px-layout-side">
           <Outlet />
         </div>
+        {showNavigation && <BottomNavigation />}
       </main>
     </div>
   );
