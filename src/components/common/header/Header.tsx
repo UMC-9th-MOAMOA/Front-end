9abd0e2d15bbd992aefd1fdc0815f94c0fa05297
Gamgyul <ryusangwan12@gmail.com>
2026-01-24 01:46:44 +0900

    Refactor: 레이아웃 수정에 따른 공통 컴포넌트 수정

--- src/components/common/header/Header.tsx
+++ src/components/common/header/Header.tsx
@@ -29,7 +29,7 @@ export default function Header({
   };
 
   return (
-    <header className="grid grid-cols-[1.5rem_1fr_1.5rem] items-center pt-28">
+    <header className="mt-28 grid w-full grid-cols-[24px_1fr_24px] items-center whitespace-nowrap">
       <IcLeft className="size-24 cursor-pointer" onClick={handleBack} />
       <h1 className="heading-2 text-center text-black">{title}</h1>
       <div className="flex justify-end">
