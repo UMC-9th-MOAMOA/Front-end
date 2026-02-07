9c17dd5957355bad02e499d4da76d76dd2fb1787
Gamgyul <ryusangwan12@gmail.com>
2026-02-08 00:05:49 +0900

    Feat: 구매/장착 API 함수 및 홈 조회 API 추가

--- src/apis/shop/shop.ts
+++ src/apis/shop/shop.ts
@@ -1,11 +1,42 @@
 import { authAPI } from "@/apis/axios";
 import type { ApiResponse } from "@/types/api/api";
-import type { ItemCategory, ShopItemsResult } from "@/types/home/shop";
+import type {
+  EquipResult,
+  ItemCategory,
+  PurchaseResult,
+  ShopItemsResult,
+} from "@/types/home/shop";
 
 export const getShopItems = async (category: ItemCategory) => {
   const { data } = await authAPI.get<ApiResponse<ShopItemsResult>>("/items", {
     params: { category },
   });
+
+  if (!data.isSuccess) {
+    throw new Error(data.message);
+  }
+
+  return data.result;
+};
+
+export const purchaseItem = async (itemId: number) => {
+  const { data } = await authAPI.post<ApiResponse<PurchaseResult>>(
+    "/members/me/purchases",
+    { itemId },
+  );
+
+  if (!data.isSuccess) {
+    throw new Error(data.message);
+  }
+
+  return data.result;
+};
+
+export const equipItem = async (itemId: number) => {
+  const { data } = await authAPI.patch<ApiResponse<EquipResult>>(
+    "/members/me/avatar/equipment",
+    { itemId },
+  );
 
   if (!data.isSuccess) {
     throw new Error(data.message);
