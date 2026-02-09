19508baccbddf0fded6a671a9921cef7287ee46b
SangwanYu <ryusangwan12@gmail.com>
2026-02-09 14:51:23 +0900

    Fix: 오타 수정

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -70,7 +70,7 @@ const routes: (RouteObject & { handle?: RouteHandle })[] = [
         handle: { bgColor: "bg-gray-50", hideBottomNav: true },
       },
       {
-        path: "m",
+        path: "mission",
         element: <MissionEntry />,
         handle: { bgColor: "bg-gray-100", hideBottomNav: true },
       },
