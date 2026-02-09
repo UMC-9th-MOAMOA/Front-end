90d19d10aa386a3b1808c7bb18875130a5e002df
Garden <chlwjddnjs6284@naver.com>
2026-02-09 23:28:29 +0900

    refactor: total 문구 UI 수정

--- src/pages/mypage/hooks/useSpaceCalendarDay.ts
+++ src/pages/mypage/hooks/useSpaceCalendarDay.ts
@@ -1,10 +1,10 @@
-import { useSuspenseQuery } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import { getSpaceCalendarDay } from "@/apis/calendar/calendar";
 
 export const useSpaceCalendarDay = (date: string) => {
-  return useSuspenseQuery({
+  return useQuery({
     queryKey: ["space", "calendar", "day", date],
     queryFn: () => getSpaceCalendarDay(date),
+    keepPreviousData: true,
   });
 };
-
