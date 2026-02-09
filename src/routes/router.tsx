11de15ab7d73f6b92dfeaacbe9bc8bffb6d506bb
Garden <chlwjddnjs6284@naver.com>
2026-02-09 12:54:49 +0900

    Feat: 내미션 API 연동 1차/ 자세히보기 UI 구현

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -6,6 +6,7 @@ import Terms from "@/pages/auth/signup/Terms";
 import HomePage from "@/pages/home/Home";
 import MissionEntry from "@/pages/mission/entry/MissionEntry";
 import QuizPage from "@/pages/mission/quiz/QuizPage";
+import CompletedMissionDetailPage from "@/pages/mypage/CompletedMissionDetailPage";
 import MyPage from "@/pages/mypage/MyPage";
 import Onboarding from "@/pages/onboarding/Onboarding";
 import Pocket from "@/pages/pocket/Pocket";
@@ -92,6 +93,11 @@ const routes: (RouteObject & { handle?: RouteHandle })[] = [
         path: "mypage",
         element: <MyPage />,
         handle: { bgColor: "bg-gray-100" },
+      },
+      {
+        path: "mypage/mission/:missionId",
+        element: <CompletedMissionDetailPage />,
+        handle: { bgColor: "bg-white", hideBottomNav: true },
       },
       {
         path: "settings",
