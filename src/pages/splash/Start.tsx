6aba37001ca24af320a52a4a6f580cda9bd24b8f
kkimsxu <jisue0567@hanyang.ac.kr>
2026-02-09 22:56:04 +0900

    Design: 다람쥐 크기 고정

--- src/pages/splash/Start.tsx
+++ src/pages/splash/Start.tsx
@@ -29,7 +29,7 @@ const StartPage = ({ enableFade = true }: StartPageProps) => {
 
   return (
     <div
-      className={`-mb-96 flex flex-1 flex-col items-center ${enableFade ? "transition-opacity duration-400" : ""} ${isVisible ? "opacity-100" : "opacity-0"} not-[]:text-center`}
+      className={`-mb-96 flex flex-1 flex-col items-center ${enableFade ? "transition-opacity duration-400" : ""} ${isVisible ? "opacity-100" : "opacity-0"} `}
     >
       <Logo className="mx-auto mt-85 block h-auto w-193" aria-label="MOAMOA" />
       <div className="flex flex-col items-center gap-20">
@@ -41,7 +41,7 @@ const StartPage = ({ enableFade = true }: StartPageProps) => {
           함께 숨겨진 시간을 찾아 생산적인 하루를 보내보세요.
         </p>
       </div>
-      <SquirrelStart className="mt-40 h-auto w-full px-7" aria-label="MOAMOA" />
+      <SquirrelStart className="mt-40 h-auto w-300 px-7" aria-hidden="true" />
       <div className="w-full">
         <Button
           type="button"
