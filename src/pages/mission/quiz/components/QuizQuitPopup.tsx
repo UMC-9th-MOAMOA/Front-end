7be3c9117a272c883f137865fed74376b37cfe7f
aeongiing <ywjk0515@gmail.com>
2026-02-10 20:09:05 +0900

    Fix: 코멘트 변경

--- src/pages/mission/quiz/components/QuizQuitPopup.tsx
+++ src/pages/mission/quiz/components/QuizQuitPopup.tsx
@@ -14,9 +14,9 @@ export default function QuizQuitPopup({
       <div className="mx-48 w-full rounded-xl bg-white px-20 pt-28 pb-20">
         <h2 className="heading-3 text-center text-red-400">잠깐 !</h2>
         <p className="body-2 mt-12 text-center text-gray-700">
-          지금 나가면 도토리 {acorns}개가 사라져요..
+          지금 나가면 얻은 도토리가 사라져요.
           <br />
-          그래도 중단하실 건가요?
+          재도전하면 도토리를 얻을 수 없어요.
         </p>
         <div className="flex gap-12 pt-32">
           <button
@@ -31,7 +31,7 @@ export default function QuizQuitPopup({
             onClick={onStay}
             className="body-2 flex-1 rounded-xl bg-moamoa-300 py-12 text-white"
           >
-            재도전하기
+            계속하기
           </button>
         </div>
       </div>
