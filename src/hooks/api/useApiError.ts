a7f6af6a0fd5a9bff6f5928cbc9467ac81991491
SangwanYu <ryusangwan12@gmail.com>
2026-02-11 11:20:35 +0900

    Feat: 인터셉터에도 403_6 에러 처리 추가

--- src/hooks/api/useApiError.ts
+++ src/hooks/api/useApiError.ts
@@ -3,7 +3,6 @@ import { storage } from "@/apis/storage";
 import { ERROR_CODES } from "@/constants/errorCodes";
 import type { ApiError } from "@/types/api/api";
 
-// 수정 및 추가 가능
 const RELOGIN_CODES: string[] = [
   ERROR_CODES.AUTH.SUSPENDED_ACCOUNT,
   ERROR_CODES.AUTH.INACTIVE_ACCOUNT,
@@ -21,20 +20,17 @@ export function useApiError(handlers?: Record<string, ErrorHandler>) {
     const code = apiError.serverCode;
 
     if (!code) return;
-
-    // 화면별 오버라이드가 있으면 우선 실행
+// 화면별 오버라이드가 있으면 우선 실행
     if (handlers?.[code]) {
       handlers[code](apiError);
       return;
     }
-
-    // 온보딩 미완료 에러 처리
+// 온보딩 미완료 에러 처리
     if (code === ERROR_CODES.AUTH.ONBOARDING_INCOMPLETE) {
       navigate("/onboarding", { replace: true });
       return;
     }
-
-    // 재로그인 필요한 상황 에러 기본 처리
+// 재로그인 필요한 상황 에러 기본 처리
     if (RELOGIN_CODES.includes(code)) {
       storage.removeToken();
       navigate("/login");
