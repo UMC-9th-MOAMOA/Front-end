bdadfe8036f8e7ff937200c09707045f8fc54f55
kkimsxu <jisue0567@hanyang.ac.kr>
2026-02-19 22:19:02 +0900

    Fix: 로그아웃 시 쿼리 캐시 삭제

--- src/pages/settings/hooks/useLogout.ts
+++ src/pages/settings/hooks/useLogout.ts
@@ -1,4 +1,4 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { logout } from "@/apis/auth/auth";
 import { useApiError } from "@/hooks/api/useApiError";
 
@@ -8,11 +8,13 @@ type LogoutHandlers = {
 
 export const useLogout = (handlers?: LogoutHandlers) => {
   const { handleError } = useApiError();
+  const queryClient = useQueryClient();
 
   return useMutation({
     mutationFn: logout,
     onSuccess: () => {
       localStorage.clear();
+      queryClient.clear();
       handlers?.onSuccess?.();
     },
     onError: (error) => {
